68ec89134cd6d42485a005d0f0e30e8aa953373d
David HL <64745261+DavidwithD@users.noreply.github.com>
2024-01-04 21:43:56 +0800

    initial

---
+++ src/pages/books/components/BookPage.tsx
@@ -0,0 +1,16 @@
+import BookProvider from "../hooks/usebooks";
+import BookList from "./BookList";
+import "../style/bookStyle.css";
+import Backup from "./Backup";
+import Creator from "../../cards/create/components/Creator";
+
+export default function BookPage() {
+  return (
+    <BookProvider>
+      <h2>Books</h2>
+      {/* <Backup /> */}
+      <BookList />
+      <Creator />
+    </BookProvider>
+  );
+}
